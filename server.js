8f681afac4b25f887a573a47c8f02b33bbb29997
SanctaGee <tonygabito@gmail.com>
2026-03-07 15:26:59 +0100

    Initial check-in for the Lesson 1 of my First project

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use('/', require('./routes'));
+
+app.listen(port, () => {
+    console.log(`Web Server is running on port ${port}`);
+});
