779bb96385aa7d0fb47d4a0abf955d8c650bf3d8
SanctaGee <tonygabito@gmail.com>
2026-03-08 11:13:59 +0100

    modify to include the database

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
+const mongodb = require('./data/database');
 const app = express();
-
 const port = process.env.PORT || 5000;
 
-app.use('/', require('./routes'));
+app.use(express.json());
 
-app.listen(port, () => {
-    console.log(`Web Server is running on port ${port}`);
+// This line connects the routes
+app.use('/', require('./routes/index'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the CSE 341 Project!');
 });
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Database is listening and node is running on port ${port}`);
+    });
+  }
+});
+
